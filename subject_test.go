73f8590674f3ab0a54a7fce8a092005594533093
Michael Teuscher <michael.teuscher@protonmail.com>
2019-01-11 22:19:12 +0100

    Added more tests

--- subject_test.go
+++ subject_test.go
@@ -19,3 +19,26 @@ func TestNewSubject(t *testing.T) {
 		t.Error("NewSubject did not create empty subscriptions map")
 	}
 }
+
+func TestSubject_Subscribe(t *testing.T) {
+	subject := NewSubject()
+	subscription := subject.Subscribe(func() {})
+
+	if _, exists := subject.Subscriptions[subscription]; !exists {
+		t.Error("Subscription is not in subscription map")
+	}
+}
+
+func TestSubject_Next(t *testing.T) {
+	subject := NewSubject()
+	didRun := false
+	subject.Subscribe(func(run bool) {
+		didRun = run
+	})
+
+	subject.Next(true)
+
+	if !didRun {
+		t.Error("Subscription handler wasnt called!")
+	}
+}
