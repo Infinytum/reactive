1ba1b6f3efb02eb9e32a4dba4c48f1fee0559bc1
Michael Teuscher <michael.teuscher@protonmail.com>
2019-01-14 11:57:58 +0100

    Fixed issue #1

--- subject.go
+++ subject.go
@@ -38,37 +38,55 @@ func (subject *Subject) Next(values ...interface{}) {
 
 func (subject Subject) notifySubscriber(subscription Subscription, values []interface{}) {
 	if fn, ok := subject.Subscriptions[subscription]; ok {
-		refFn := reflect.ValueOf(fn)
+		refFn := reflect.TypeOf(fn)
 		fnArgs := make([]reflect.Value, 0)
 
-		for i := 0; i < refFn.Type().NumIn(); i++ {
-			val := values[i]
+		for argIndex := 0; argIndex < refFn.NumIn(); argIndex++ {
+			providedVal := values[argIndex]
 
-			if val == nil {
-				fnArgs = append(fnArgs, reflect.New(refFn.Type().In(i)).Elem())
-			} else {
-				// issue 1
-				if refFn.Type().In(i).Kind() == reflect.Slice {
-					for _, innerVal := range values[i:len(values)] {
-						fnArgs = append(fnArgs, reflect.ValueOf(innerVal))
+			// Slice arguments need special treatment
+			if refFn.In(argIndex).Kind() == reflect.Slice {
+				sliceType := refFn.In(argIndex).Elem()
+
+				for _, innerVal := range values[argIndex:len(values)] {
+					if providedVal == nil {
+						fnArgs = append(fnArgs, reflect.New(sliceType).Elem())
+						continue
 					}
-					break
-				} else {
-					fnArgs = append(fnArgs, reflect.ValueOf(val))
+
+					if !reflect.TypeOf(innerVal).AssignableTo(sliceType) {
+						// Slice does not match received data, skipping this subscriber
+						return
+					}
+					fnArgs = append(fnArgs, reflect.ValueOf(innerVal))
 				}
-			}
+				// Finish loop as we have filled in all data to the slice
+				break
+			} else {
+				argType := refFn.In(argIndex)
+				if providedVal == nil {
+					values[argIndex] = reflect.New(argType).Elem()
+					providedVal = values[argIndex]
+				}
 
-			if val != nil && !reflect.ValueOf(values[i]).Type().AssignableTo(refFn.Type().In(i)) {
-				return
-			}
+				if !reflect.TypeOf(providedVal).AssignableTo(argType) {
+					// Method signature not compatible with this input. Skipping subscriber
+					return
+				}
 
-			if refFn.Type().NumIn() != len(fnArgs) {
-				return
+				fnArgs = append(fnArgs, reflect.ValueOf(values[argIndex]))
+
+				if argIndex == refFn.NumIn()-1 {
+					if refFn.NumIn() != len(fnArgs) {
+						// Skipping non-slice overflow
+						return
+					}
+				}
 			}
 
 		}
 
-		refFn.Call(fnArgs)
+		reflect.ValueOf(fn).Call(fnArgs)
 	}
 }
 
